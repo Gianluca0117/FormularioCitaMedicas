7af7f73923b22b135f67f14bed7bf59de39455e9
Gianluca0117 <gugalde3775@utm.edu.ec>
2024-11-21 23:47:23 -0500

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -109,3 +109,11 @@ function mostrarNotificacion(mensaje) {
         }, 500);
     }, 3000);
 }
+
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.addEventListener('click', () => {
+  navLinks.classList.toggle('active');  // Mostrar u ocultar el menú
+  menuIcon.classList.toggle('active');  // Activar la animación de las rayas
+});
